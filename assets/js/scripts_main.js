0ac78d6bda7b46d8f4fe36769e32c47d01aa459d
guijocede <guijocede@gmail.com>
2025-03-06 09:40:42 -0400

    Agregando cambios paleta de colores

--- assets/js/scripts_main.js
+++ assets/js/scripts_main.js
@@ -23,6 +23,7 @@ function chatSystem() {
   chatWindow.addEventListener("click", () => {
     // Chat window display none click afuera
     chatWindow.style.display = "none";
+    chatWindow.stopPropagation();
   });
   // Stop Propagation
   chatMessages.addEventListener("click", (event) => {
@@ -47,8 +48,18 @@ function chatSystem() {
   });
 }
 
+function cargaRecetas() {
+  document.addEventListener("click", function (event) {
+    if (event.target.classList.contains("history-msg")) {
+      const idMensaje = event.target.id;
+      cargarRecetaMain(idMensaje);
+    }
+  });
+}
+
 document.addEventListener("DOMContentLoaded", (e) => {
   chatSystem();
+  cargaRecetas();
 });
 
 // async function consultarAPI(itemId, query) {
@@ -71,7 +82,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
 //---------------------------------------------------------------
 
-async function obtenerDatos(cadenaTexto) {
+async function cargarRecetaMain(cadenaTexto) {
   try {
     const response = await fetch(
       `http://127.0.0.1:8000/obtener_ejemplo?cadena_texto=${cadenaTexto}`
@@ -79,7 +90,8 @@ async function obtenerDatos(cadenaTexto) {
     const datos = await response.json();
 
     // Imprimir el objeto JSON completo
-    console.log(datos);
+    crearTab("tabs-head", datos.nombre);
+    cargarJsonMain("main-content", datos);
 
     // Imprimir cada clave con su valor
     for (const clave in datos) {
@@ -95,4 +107,72 @@ async function obtenerDatos(cadenaTexto) {
 
 // Ejemplo de uso:
 const texto = "Texto de ejemplo";
-console.log(obtenerDatos(texto));
+
+function cargarJsonMain(idContenedor, recetaJson) {
+  $contenedor = document.getElementById(idContenedor);
+  if ($contenedor)
+    $contenedor.innerHTML = `<div class="main-zone">
+          <div class="main-left">
+            <div class="img-container">
+              <img src="${recetaJson.imagenURL}" alt="" />
+            </div>
+            <div class="history-msg main-nutri-card">
+              <div class="nutri-values">
+                <div class="values">
+                  <span class="total-cal">${recetaJson.calorias}Kcal</span>
+                  <span class="fw-big"
+                    ><span>Prot</span> <span>${recetaJson.proteina}gr</span></span
+                  >
+                  <span class="fw-big"
+                    ><span>Carbs</span> <span>${recetaJson.carbohidratos}gr</span></span
+                  >
+                  <span class="fw-big"
+                    ><span>Grasa</span> <span>${recetaJson.grasa}gr</span></span
+                  >
+                  <span><span>Colesterol</span> <span>${recetaJson.colesterol}mg</span></span>
+                  <span><span>Sodio</span> <span>${recetaJson.sodio}mg</span></span>
+                  <span><span>Vitamina A</span> <span>${recetaJson.vitaminaA}mcg</span></span>
+                  <span><span>Vitamina C</span> <span>${recetaJson.vitaminaC}mg</span></span>
+                  <span><span>Vitamina D</span> <span>${recetaJson.vitaminaD}mcg</span></span>
+                  <span><span>Calcio</span> <span>${recetaJson.calcio}mg</span></span>
+                  <span><span>Hierro</span> <span>${recetaJson.hierro}mg</span></span>
+                  <span><span>Potasio</span> <span>${recetaJson.potasio}mg</span></span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="main-right">
+            <h2>TÍTULO DE LA RECETA</h2>
+            <ul id="ingredientes">
+              <li>Ingrediente1</li>
+              <li>Ingrediente2</li>
+              <li>Ingrediente3</li>
+              <li>Ingrediente4</li>
+              <li>Ingrediente5</li>
+              <li>Ingrediente6</li>
+              <li>Ingrediente7</li>
+              <li>Ingrediente8</li>
+              <li>Ingrediente9</li>
+              <li>Ingrediente10</li>
+            </ul>
+          </div>
+        </div>
+      </div>`;
+}
+
+function crearTab(idContenedor, titulo) {
+  const $contenedor = document.getElementById(idContenedor);
+
+  if ($contenedor) {
+    const nuevoTab = document.createElement("div");
+    nuevoTab.className = "tab";
+
+    const tituloTab = document.createElement("h2");
+    tituloTab.textContent = titulo;
+
+    nuevoTab.appendChild(tituloTab);
+    $contenedor.appendChild(nuevoTab);
+  } else {
+    console.error(`No se encontró el contenedor con ID: ${idContenedor}`);
+  }
+}
